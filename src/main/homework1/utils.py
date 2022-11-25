a8ef676e5291f38031d0f8e2ffdfcfaf07c939e8
Temur <dearpery@gmail.com>
2022-11-25 14:37:09 +0300

    change utils file

--- src/main/homework1/utils.py
+++ src/main/homework1/utils.py
@@ -1,4 +1,6 @@
 from PyQt5.QtWidgets import QDesktopWidget
+import numpy as np
+from PyQt5.QtGui import QPixmap, QImage
 
 
 def center(item):
@@ -6,3 +8,20 @@ def center(item):
     cp = QDesktopWidget().availableGeometry().center()
     qr.moveCenter(cp)
     item.move(qr.topLeft())
+
+
+def pixmap_to_image_with_format(pixmap):
+    width = pixmap.width()
+    height = pixmap.height()
+
+    image = pixmap.toImage()
+    channels = 4
+    bit_string = image.bits().asstring(width * height * channels)
+
+    return np.frombuffer(bit_string, dtype=np.uint8).reshape((height, width, channels)), image.format()
+
+
+def matrix_to_pixmap(matrix, image_format):
+    height, width, _ = matrix.shape
+
+    return QPixmap(QImage(matrix.data, width, height, 4 * width, image_format))
