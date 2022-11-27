7e7f15c4a2cf7c4853db10b4c773c38fde325500
Temur <dearpery@gmail.com>
2022-11-27 17:47:44 +0300

    added tests for algorithm

---
+++ src/test/homework2/task1/bfs.py
@@ -0,0 +1,37 @@
+import unittest
+from src.main.homework2.task1.bfs import bfs
+
+
+class Bfs(unittest.TestCase):
+    def test_bfs_with_acyclic_graph(self):
+        matrix_info = [[0, 0, 1, 1, 5],
+                       [1, 4, 3, 5, 2],
+                       [1, 1, 2, 4, 1]]
+        start = 0
+
+        expect = [1, 2, 4, 3, 2, 3]
+        result = bfs(matrix_info, start).to_lists()[1]
+
+        self.assertListEqual(expect, result)
+
+    def test_bfs_with_cyclic_graph(self):
+        matrix_info = [[0, 0, 1, 1, 5, 1, 4],
+                       [1, 4, 3, 5, 2, 0, 1],
+                       [7, 7, 1, 12, 1, 1, 1]]
+        start = 0
+
+        expect = [1, 2, 4, 3, 2, 3]
+        result = bfs(matrix_info, start).to_lists()[1]
+
+        self.assertListEqual(expect, result)
+
+    def test_bfs_with_graph_with_loops(self):
+        matrix_info = [[0, 0, 1, 1, 5, 4],
+                       [1, 4, 3, 5, 2, 4],
+                       [1, 1, 1, 3, 1, 1]]
+        start = 0
+
+        expect = [1, 2, 4, 3, 2, 3]
+        result = bfs(matrix_info, start).to_lists()[1]
+
+        self.assertListEqual(expect, result)
---
+++ src/test/homework2/task1/triangles_count.py
@@ -0,0 +1,34 @@
+import unittest
+from src.main.homework2.task1.triangles_count import triangles_count
+
+
+class TriangleCount(unittest.TestCase):
+    def test_triangle_count_with_graph_without_triangle(self):
+        matrix_info = [[0, 0, 1, 2, 2, 2, 3, 4],
+                       [1, 2, 0, 0, 3, 4, 2, 2],
+                       [1, 1, 1, 1, 1, 1, 1, 1]]
+
+        expect = 0
+        result = triangles_count(matrix_info)
+
+        self.assertEqual(expect, result)
+
+    def test_triangle_count_with_triangles(self):
+        matrix_info = [[0, 0, 1, 2, 2, 2, 3, 4, 1, 2, 3, 4],
+                       [1, 2, 0, 0, 3, 4, 2, 2, 2, 1, 4, 3],
+                       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+        expect = 2
+        result = triangles_count(matrix_info)
+
+        self.assertEqual(expect, result)
+
+    def test_triangle_count_with_two_general_nodes_for_triangles(self):
+        matrix_info = [[0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4],
+                       [1, 2, 3, 4, 0, 2, 3, 4, 0, 1, 0, 1, 0, 1],
+                       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+        expect = 3
+        result = triangles_count(matrix_info)
+
+        self.assertEqual(expect, result)
