80a82d426d30e9ee2345d7d85a7ecbf0936bd472
Temur <dearpery@gmail.com>
2022-11-27 12:08:39 +0300

    deleted repeating code(HW1 branch)

--- src/main/homework1/run.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from main_view import MainView
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    MainView().show()
-    sys.exit(app.exec_())
