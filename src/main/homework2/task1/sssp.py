444fec30a1c8a47aa44b9ecc71196da7a0b1f782
Temur <dearpery@gmail.com>
2022-12-02 00:46:26 +0300

    update sssp method

--- src/main/homework2/task1/sssp.py
+++ src/main/homework2/task1/sssp.py
@@ -18,7 +18,8 @@ def sssp(m, start):
         result[start] = 0
 
         for i in range(matrix.nrows-1):
-            result.min_plus(matrix, out=result, accum=INT64.min)
+            if result == result.min_plus(matrix, out=result, accum=INT64.min):
+                break
 
         return result
 
